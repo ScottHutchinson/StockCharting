579d3b1959386040504f1addfaa1a494f11c6e60
ScottHutchinson <scotthutchinson@usa.net>
2019-04-03 16:24:11 -0700

    Adding a TextAnnotation to the point where the user right-clicks.

--- StockCharting/Form1.cs
+++ StockCharting/Form1.cs
@@ -24,11 +24,37 @@ namespace StockCharting {
             chart1.ChartAreas[0].AxisY.Minimum = 180;
         }
 
+        private int getNearestIdx(double x, List<double> xValues) {
+            var idx = xValues.BinarySearch(x);
+            if (Math.Abs(idx) > xValues.Count - 1) {
+                return xValues.Count - 1;
+            }
+            else if (idx < 0) {
+                var nextIdx = ~idx;
+                if (nextIdx == 0) {
+                    return 0;
+                }
+                else {
+                    var lower = xValues[nextIdx - 1];
+                    var higher = xValues[nextIdx];
+                    var nearestIdx = Math.Abs(lower - x) < Math.Abs(higher - x) ? nextIdx - 1 : nextIdx;
+                    return nearestIdx;
+                }
+            }
+            else {
+                return idx; // exact hit
+            }
+        }
+
         private void chart1_MouseClick(object sender, MouseEventArgs e) {
             if (e.Button == MouseButtons.Right) {
-                TextAnnotation textAnnotation = new TextAnnotation();
-                textAnnotation.Text = "Hmmm...";
-                textAnnotation.SetAnchor(chart1.Series[0].Points[4]);
+                var x = chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.X);
+                var xValues = chart1.Series[0].Points.Select(p => p.XValue).ToList();
+                var nearestIdx = getNearestIdx(x, xValues);
+                var textAnnotation = new TextAnnotation {
+                    Text = "Hmmm..." // TODO: Prompt the user for the text.
+                };
+                textAnnotation.SetAnchor(chart1.Series[0].Points[nearestIdx]);
                 chart1.Annotations.Add(textAnnotation);
             }
         }
